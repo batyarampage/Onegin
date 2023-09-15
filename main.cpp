3b0452f7c34cc12f354af73b82412108b7f70abb
batyarampage <137216948+batyarampage@users.noreply.github.com>
2023-09-15 15:59:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,22 +9,36 @@
 #include <ctype.h>
 
 
+struct reading_from_file {
+
+    size_t filesize = 0;
+
+    char* file_name = nullptr;
+
+    char* strings_of_the_file = nullptr;
+
+    char** ptr_on_enter_in_strings = nullptr;
+
+    size_t count_of_lines_in_file = 1;
+
+};
+
 
 size_t my_strlen (const char* str, size_t* curr_symb);
 
-size_t filesize (char file_name[]);
+void filesize (struct reading_from_file* parameters_of_file);
 
-void read_text_from_tile (size_t filesize, char file_name[], char* strings_of_file);
+void read_text_from_tile (struct reading_from_file* parameters_of_file);
 
-void whitespace_hatching_and_replacing_on_0 (char* strings_of_file, size_t curr_file_size,
-                                             size_t curr_size_of_ptr_on_enter,char** ptr_on_enter_in_strings,
-                                             size_t* curr_line);
+void whitespace_hatching_and_replacing_on_0 (struct reading_from_file* parameters_of_file);
 
 int my_reversed_strcmp (const void* str1, const void* str2);
 
-void my_qsort (char** data, size_t curr_strlen, size_t size_of_type, int (*comparator));
-
 int my_strcmp_without_punct (const void* strings1, const void* strings2);
+
+void read_sort_and_file_output (struct reading_from_file* parameters_of_file);
+
+
 
 
 
@@ -45,7 +59,7 @@ size_t my_strlen (const char* str, size_t* curr_symb){
     return lenght;
 }
 
-int my_reversed_strcmp (const void* strings1, const void* strings2){
+int my_strcmp_without_punctuation_Reversed (const void* strings1, const void* strings2){
 
     const char* str1 = (const char*) strings1;
     const char* str2 = (const char*) strings2;
@@ -126,69 +140,239 @@ int my_reversed_strcmp (const void* strings1, const void* strings2){
 
 
 
-int my_strcmp_without_punct (const void* strings1, const void* strings2){
-
-    assert(strings1);
-    assert(strings2);
+int my_strcmp_without_punctuation_Direct (const void* strings1, const void* strings2){
 
     const char* str1 = (const char*) strings1;
     const char* str2 = (const char*) strings2;
 
+    size_t len_of_str1 = 0;
+    size_t len_of_str2 = 0;
+
+    size_t len_of_str1_without_spec_symb = my_strlen(str1, &len_of_str1);
+    size_t len_of_str2_without_spec_symb = my_strlen(str2, &len_of_str2);
+
+    //printf("len_of_str1_without_spec %u\n",len_of_str1_without_spec);
+    //printf("len_of_str2_without_spec %u\n",len_of_str2_without_spec);
+    //printf("len_of_str1 %u\n",len_of_str1);
+    //printf("len_of_str2 %u\n",len_of_str2);
+
+    if (len_of_str1_without_spec_symb > len_of_str2_without_spec_symb){
+
+        return 1;
+    }
+
+    else if (len_of_str1_without_spec_symb < len_of_str2_without_spec_symb){
+
+        return -1;
+    }
+
+    else {
+
+        size_t iterator_str1 = 0;
+        size_t iterator_str2 = 0;
+
+        while ((*(str1 + iterator_str1)) && (*(str2 + iterator_str2)) && (iterator_str1 < len_of_str1) && (iterator_str2 < len_of_str2)){
+
+            while ((!isalpha(*(str1 + iterator_str1))) && (*(str1 + iterator_str1)) && (iterator_str1 < len_of_str1)){
+
+                iterator_str1++;
+            }
+
+            while ((!isalpha(*(str2 + iterator_str2))) && (*(str2 + iterator_str2)) && (iterator_str2 < len_of_str2)){
+
+                iterator_str2++;
+            }
+
+            int difference = (*(str1 + iterator_str1)) - (*(str2 + iterator_str2));
+
+            //printf("*(str1) %c\n", *(str1 + iterator_str1));
+            //printf("*(str2) %c\n", *(str2 + iterator_str2));
+            //printf("----------------------------------\n");
+
+            if (difference > 0){
+
+                return 1;
+            }
+
+            if (difference < 0){
+
+                return -1;
+            }
+
+            iterator_str1++;
+            iterator_str2++;
+        }
+
+        if ((*str1) > (*str2)){
+
+            return 1;
+        }
+
+        if ((*str1) < (*str2)){
+
+            return -1;
+        }
+
+        return 0;
+    }
+
 
 }
 
 
+int my_strcmp_with_punctuation_Direct (const void* strings1, const void* strings2){
 
-size_t filesize (char file_name[]){
+    const char* str1 = (const char*) strings1;
+    const char* str2 = (const char*) strings2;
+
+    size_t len_of_str1 = 0;
+    size_t len_of_str2 = 0;
+
+    size_t len_of_str1_without_spec_symb = my_strlen(str1, &len_of_str1);
+    size_t len_of_str2_without_spec_symb = my_strlen(str2, &len_of_str2);
+
+    //printf("len_of_str1_without_spec %u\n",len_of_str1_without_spec_symb);
+    //printf("len_of_str2_without_spec %u\n",len_of_str2_without_spec_symb);
+    //printf("len_of_str1 %u\n",len_of_str1);
+    //printf("len_of_str2 %u\n",len_of_str2);
+
+    size_t iterator_str1 = 0;
+    size_t iterator_str2 = 0;
+
+    while ((*(str1 + iterator_str1)) && (*(str2 + iterator_str2)) && (iterator_str1 < len_of_str1) && (iterator_str2 < len_of_str2)){
+
+        while ((!isalpha(*(str1 + iterator_str1))) && (*(str1 + iterator_str1)) && (iterator_str1 < len_of_str1)){
+
+            iterator_str1++;
+        }
+
+        while ((!isalpha(*(str2 + iterator_str2))) && (*(str2 + iterator_str2)) && (iterator_str2 < len_of_str2)){
+
+            iterator_str2++;
+        }
+
+        int difference = (*(str1 + iterator_str1)) - (*(str2 + iterator_str2));
+
+        printf("*(str1) %c\n", *(str1 + iterator_str1));
+        printf("*(str2) %c\n", *(str2 + iterator_str2));
+        printf("diff %d\n",difference);
+        printf("----------------------------------\n");
+
+        if (difference > 0){
+
+            return 1;
+        }
+
+        if (difference < 0){
+
+            return -1;
+        }
+
+        iterator_str1++;
+        iterator_str2++;
+    }
+    printf("iter1 %u\n", iterator_str1);
+    printf("iter2 %u\n", iterator_str2);
+
+    if (iterator_str2 < len_of_str2){
+
+        return -1;
+    }
+
+    if (iterator_str1 < len_of_str1){
+
+        return 1;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+void filesize (struct reading_from_file* parameters_of_file){
 
     struct stat st = {};
-    stat(file_name, &st);
+    stat(parameters_of_file->file_name, &st);
 
     size_t size_of_file = st.st_size;
 
-    printf("%u\n", size_of_file);
-
-    return size_of_file;
+    parameters_of_file->filesize = size_of_file;
 }
 
-void read_text_from_tile (size_t filesize, char file_name[], char* strings_of_file){
 
-    FILE *file = fopen(file_name, "r");
 
-    fread(strings_of_file, (size_t) filesize, 1, file);
+void read_text_from_tile (struct reading_from_file* parameters_of_file){
+
+    FILE *file = fopen(parameters_of_file->file_name, "r");
+
+    fread(parameters_of_file->strings_of_the_file, (size_t) parameters_of_file->filesize, 1, file);
 
     fclose(file);
 }
 
-void whitespace_hatching_and_replacing_on_0 (char* strings_of_file, size_t curr_file_size,
-                                             size_t curr_size_of_ptr_on_enter,char** ptr_on_enter_in_strings,
-                                             size_t* curr_line){
 
-    assert(ptr_on_enter_in_strings != nullptr);
-    assert(strings_of_file != nullptr);
 
-    *ptr_on_enter_in_strings = strings_of_file;
+void whitespace_hatching_and_replacing_on_0 (struct reading_from_file* parameters_of_file){
 
-    for (size_t i = 0; i < curr_file_size; i++){
+    *(parameters_of_file->ptr_on_enter_in_strings) = parameters_of_file->strings_of_the_file;
 
-        if ((*(strings_of_file + i)) == '\n'){
+    for (size_t i = 0; i < parameters_of_file->filesize; i++){
 
-            /*if (curr_line + 1 >= *curr_size_of_ptr_on_enter){
+        if ((*(parameters_of_file->strings_of_the_file + i)) == '\n'){
 
-                *curr_size_of_ptr_on_enter *= 2;
+            *(parameters_of_file->ptr_on_enter_in_strings + (parameters_of_file->count_of_lines_in_file)) = (parameters_of_file->strings_of_the_file + i + 1);
 
-                ptr_on_enter_in_strings = (char**) realloc (ptr_on_enter_in_strings, (*curr_size_of_ptr_on_enter) * sizeof(char*));
-            }*/
+            (parameters_of_file->count_of_lines_in_file)++;
 
-            *(ptr_on_enter_in_strings + (*curr_line)) = (strings_of_file + i + 1);
-
-            (*curr_line)++;
-
-            *(strings_of_file + i) = '\0';
+            *(parameters_of_file->strings_of_the_file + i) = '\0';
         }
     }
 }
 
+void printer_of_file(struct reading_from_file* parameters_of_file){
+
+    for (size_t i = 0; i < parameters_of_file->count_of_lines_in_file; i++){
+
+        printf("%s\n", *(parameters_of_file->ptr_on_enter_in_strings + i));
+    }
+}
+
+void destruct_dynamic_elements (char** ptr_on_enter_in_strings, char* strings_of_file, size_t count_of_lines){
+
+    free(strings_of_file);
+
+    for (size_t i = 0; i < count_of_lines; i++){
+
+        free(*(ptr_on_enter_in_strings + i));
+    }
+
+    free(ptr_on_enter_in_strings);
+}
+
+void read_sort_and_file_output (struct reading_from_file* parameters_of_file){
+
+    filesize(parameters_of_file);
+
+    char* strings_of_file = (char *) calloc(parameters_of_file->filesize, sizeof(char));
+
+    parameters_of_file->strings_of_the_file = strings_of_file;
+
+    read_text_from_tile (parameters_of_file);
+
+    char** ptr_on_enter_in_strings = (char **) calloc(parameters_of_file->filesize, sizeof(char));
+
+    parameters_of_file->ptr_on_enter_in_strings = ptr_on_enter_in_strings;
+
+    whitespace_hatching_and_replacing_on_0 (parameters_of_file);
+
+    //printer_of_file(parameters_of_file);
+
+    destruct_dynamic_elements(ptr_on_enter_in_strings, strings_of_file, parameters_of_file->count_of_lines_in_file);
+
+}
 
 
 
@@ -196,68 +380,30 @@ void whitespace_hatching_and_replacing_on_0 (char* strings_of_file, size_t curr_
 
 int main (){
 
+    /*struct reading_from_file parameters_of_file = {};
+
     char file_name[] = "onegin1.txt";
-    size_t curr_file_size = filesize(file_name);
 
-    char* strings_of_file = (char *) calloc(curr_file_size, sizeof(char));
-    read_text_from_tile(curr_file_size, file_name,strings_of_file);
+    parameters_of_file.file_name = file_name;
 
-    char** ptr_on_enter_in_strings = (char **) calloc(curr_file_size, sizeof(char));
+    read_sort_and_file_output (&parameters_of_file);*/
 
-    size_t curr_line = 1;
-    whitespace_hatching_and_replacing_on_0 (strings_of_file, curr_file_size,
-                                            curr_file_size, ptr_on_enter_in_strings,
-                                            &curr_line);
-
-
-
-
-
-    for (size_t i = 0; i < curr_line; i++){
-
-        printf("%s\n", *(ptr_on_enter_in_strings+i));
-    }
-
-    qsort(ptr_on_enter_in_strings, curr_line, sizeof(char*), my_reversed_strcmp);
-
-    for (size_t i = 0; i < curr_line; i++){
-
-        printf("%s\n", *(ptr_on_enter_in_strings+i));
-    }
-
-
-    free(strings_of_file);
-    free(ptr_on_enter_in_strings);
-
-
-
-
-    /*char str1[] = ",ramp,ageless,";
-    char str2[] = "rampage,,,,,,,,,,,,,,,, ,,lesss,,";
-    char str3[] = "vvver";
-    char str4[] = "vertu";
-    char str5[] = "sdfhsiuf";
+    char str1[] = "rampageless";
+    char str2[] = "difference";
+    char str3[] = "rampagelezs";
+    char str4[] = "sjsdhfo";
+    char str5[] = "zxc";
 
     char* strs[5] = {str1, str2, str3, str4, str5};
 
-    for (int i = 0; i < 5; i++){
+    qsort(strs,5, sizeof(char*),my_strcmp_with_punctuation_Direct);
+
+    for (int i = 0; i<5;i++){
 
         printf("%s\n", strs[i]);
     }
 
-    printf("--------------------\n");
-
-    qsort(strs, 5, sizeof(char*),my_reversed_strcmp);
-
-    for (int i = 0; i < 5; i++){
-
-        printf("%s\n", strs[i]);
-    }*/
-
-
-
-
-    //printf("%d", (long long) (str[0]+str[1]+str[2]+str[3]+str[4]+str[5]+str[6]+str[7] + str[8]));
+    //printf("%d\n", my_strcmp_with_punctuation_Direct(str2,str3));
 
     return 0;
 }
